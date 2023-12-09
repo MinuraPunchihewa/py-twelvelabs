1fd6e11341c2b86fabc49ae0f731398b850a4a06
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-12-09 17:49:18 +0530

    added a basic model for Index

---
+++ py_twelvelabs/models/index.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Extra
+from typing import Text, List, Dict, Optional, Any
+
+
+class Index(BaseModel):
+    """
+    Index model.
+    """
+
+    _id: Text
+    index_name: Text
+    index_options: List[Text]
+    created_at: Text
+    updated_at: Text
+    engine_id: Text
+    video_count: int
+    total_duration: int
+    addons: List[Text]
