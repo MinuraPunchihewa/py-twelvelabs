2b2d8eef6199d5ef9123e5ac4bfa08bea877e6be
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-12-09 22:33:48 +0530

    updated data types of datetime fields in Index

--- py_twelvelabs/models/index.py
+++ py_twelvelabs/models/index.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, Extra
+from datetime import datetime
+from pydantic import BaseModel, validator
 from typing import Text, List, Dict, Optional, Any
 
 
@@ -10,9 +11,13 @@ class Index(BaseModel):
     _id: Text
     index_name: Text
     index_options: List[Text]
-    created_at: Text
-    updated_at: Text
+    created_at: datetime
+    updated_at: datetime
     engine_id: Text
     video_count: int
     total_duration: float
     addons: List[Text]
+
+    @validator("created_at", "updated_at", pre=True, allow_reuse=True)
+    def parse_date(cls, value):
+        return datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ")
