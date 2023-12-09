336b6bf8e7d4e66541150473d5ee3f4e656880e0
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-12-09 23:18:45 +0530

    added the skeleton for the test module

---
+++ tests/test_py_twelvelabs.py
@@ -0,0 +1,28 @@
+import unittest
+
+from py_twelvelabs import TwelveLabsAPIClient
+
+
+class TestPyTwelveLabs(unittest.TestCase):
+    """
+    Test py_twelvelabs.
+    """
+
+    def setUp(self):
+        """
+        Set up test fixtures.
+        """
+
+        self.client = TwelveLabsAPIClient()
+
+    def test_1_create_index(self):
+        pass
+
+    def test_2_get_index(self):
+        pass
+
+    def test_3_update_index_name(self):
+        pass
+
+    def test_4_delete_index(self):
+        pass
