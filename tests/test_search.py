98d0ae927851201d63c894a4480fed030555ac46
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-01-08 00:03:08 +0530

    added the unit test for query in Search

--- tests/test_search.py
+++ tests/test_search.py
@@ -3,7 +3,7 @@ import unittest
 from py_twelvelabs import TwelveLabsAPIClient
 from py_twelvelabs.utilities import get_logger
 
-from tests.utilities import IndexCreator
+from tests.utilities import IndexCreator, TaskCreator
 
 
 class TestSearch(unittest.TestCase):
@@ -18,10 +18,25 @@ class TestSearch(unittest.TestCase):
         """
 
         cls.client = TwelveLabsAPIClient()
-        cls.index_id = IndexCreator.create_index()
-        # TODO: create task
+        # cls.index_id = IndexCreator.create_index()
+        # cls.task_id = TaskCreator.create_task(cls.index_id)
 
         cls.logger = get_logger(__name__)
 
     def test_1_query(self):
-        pass
+        """
+        Test query.
+        """
+
+        results = self.client.search.query(
+            '659aec336bb8e7df9af17e47',
+            "Dog getting excited",
+            ["visual", "conversation", "text_in_video", "logo"],
+        )
+        self.logger.info(f"Response: {results}")
+
+        self.assertIsNotNone(results)
+
+
+if __name__ == "__main__":
+    unittest.main()
