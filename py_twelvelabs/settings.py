abf2140253af867b4f2f332fc2e0c52568dce6a6
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-01-07 19:29:51 +0530

    added TASK_STATUS_POLLING_INTERVAL to settings

--- py_twelvelabs/settings.py
+++ py_twelvelabs/settings.py
@@ -11,6 +11,7 @@ class Settings(BaseSettings):
     BASE_API_URL: Text = "https://api.twelvelabs.io"
     API_VERSION: Text = "v1.1"
     DEFAULT_ENGINE: Text = "marengo2.5"
+    TASK_STATUS_POLLING_INTERVAL: int = 5
 
 
 settings = Settings()
