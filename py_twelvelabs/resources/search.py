c67a17b5a27e5d1dc2f9a05a6a9a1a1c88cf70f3
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-01-07 19:06:39 +0530

    added methods to get a specific page of search results and all of them

--- py_twelvelabs/resources/search.py
+++ py_twelvelabs/resources/search.py
@@ -10,7 +10,7 @@ class SearchResource:
     # TODO: add the filter parameter
     def query(self, index_id: Text, search_query: Text, search_options: List[Text], group_by: Optional[Text] = "clip", threshold: Optional[Text] = "low", sort_option: Text = "score", operator: Text = "or", conversation_option: Text = "semantic", page_limit: int = 10):
         """
-        Query an index.
+        Query an index and return the first page of results.
 
         :param index_id: Index ID.
         :param search_query: Search query.
@@ -36,23 +36,53 @@ class SearchResource:
             "page_limit": page_limit,
         }
 
-        results = []
-
         response = self.client.submit_request("search", method="POST", data=data)
         result = response.json()
 
         if response.status_code == 200:
-            results.extend(result['data'])
+            return result
         else:
             raise APIRequestError(f"Failed to query index {index_id}: {result['message']}")
         
-        while('next_page_token' in result['page_info']):
-            response = self.client.submit_request(method="GET", endpoint=f"search/{result['page_info']['next_page_token']}")
-            result = response.json()
+    def get_search_result_page(self, page_token: Text):
+        """
+        Get a search result page.
 
-            if response.status_code == 200:
-                results.extend(result['data'])
-            else:
-                raise APIRequestError(f"Failed to query index {index_id}: {result['message']}")
+        :param page_token: Page token.
+        :return: Search result page.
+        """
+
+        response = self.client.submit_request(method="GET", endpoint=f"search/{page_token}")
+        result = response.json()
+
+        if response.status_code == 200:
+            return result
+        else:
+            raise APIRequestError(f"Failed to get search result page: {result['message']}")
+    
+    # TODO: add the filter parameter
+    def query_all(self, index_id: Text, search_query: Text, search_options: List[Text], group_by: Optional[Text] = "clip", threshold: Optional[Text] = "low", sort_option: Text = "score", operator: Text = "or", conversation_option: Text = "semantic", page_limit: int = 10):
+        """
+        Query an index and return all pages of results.
+
+        :param index_id: Index ID.
+        :param search_query: Search query.
+        :param search_options: Search options.
+        :param group_by: Group by.
+        :param threshold: Threshold.
+        :param sort_option: Sort option.
+        :param operator: Operator.
+        :param conversation_option: Conversation option.
+        :param page_limit: Page limit.
+        :return: Query result.
+        """
+
+        results = []
+
+        result = self.query(index_id=index_id, search_query=search_query, search_options=search_options, group_by=group_by, threshold=threshold, sort_option=sort_option, operator=operator, conversation_option=conversation_option, page_limit=page_limit)
+        
+        while('next_page_token' in result['page_info']):
+            result = self.get_search_result_page(page_token=result['page_info']['next_page_token'])
+            results.append(result)
             
         return results
