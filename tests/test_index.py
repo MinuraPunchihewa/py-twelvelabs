a9007be5d70ef62f6cae03a70803c2b88874f178
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-01-08 18:53:43 +0530

    updated unit tests for index to run independently

--- tests/test_index.py
+++ tests/test_index.py
@@ -7,7 +7,6 @@ from py_twelvelabs import TwelveLabsAPIClient
 from py_twelvelabs.utilities import get_logger
 
 
-# TODO: update all tests to run independently
 class TestIndex(unittest.TestCase):
     """
     Test index resource.
@@ -19,67 +18,54 @@ class TestIndex(unittest.TestCase):
         Set up test fixtures.
         """
 
-        cls.client = TwelveLabsAPIClient()
-        cls.index_name = f"test_index_{uuid.uuid4()}"
-        cls.index_id = None
-
         cls.logger = get_logger(__name__)
-    
-    @classmethod
-    def _set_index_id(cls, index_id: Text):
-        """
-        Set index ID.
 
-        :param index_id: Index ID.
+    def setUp(self):
+        """
+        Set up test fixtures for each test: initialize client, generate a random name for an index and create an index using that name.
         """
 
-        cls.index_id = index_id
-    
-    @classmethod
-    def _get_index_id(cls) -> Text:
-        """
-        Get index ID.
+        self.client = TwelveLabsAPIClient()
+        self.index_name = f"test_index_{uuid.uuid4()}"
+        self.index_id = self.client.index.create(
+            self.index_name,
+            ["visual", "conversation", "text_in_video", "logo"]
+        )
 
-        :return: Index ID.
+    def tearDown(self):
+        """
+        Tear down test fixtures after each test: delete the created index.
         """
 
-        return cls.index_id    
+        self.client.index.delete(self.index_id)
 
     def test_1_create_index(self):
         """
         Test create index.
         """
 
-        index_id = self.client.index.create(
-            self.index_name,
-            ["visual", "conversation", "text_in_video", "logo"]
-        )
-        self.logger.debug(f"Index ID: {index_id}")
-
-        self.assertIsNotNone(index_id)
-        TestIndex._set_index_id(index_id)
+        self.assertIsNotNone(self.index_id)
 
     def test_2_get_index(self):
         """
         Test get index.
         """
 
-        index = self.client.index.get(TestIndex._get_index_id())
+        index = self.client.index.get(self.index_id)
         self.logger.debug(f"Index: {index}")
 
         self.assertIsInstance(index, Index)
-        self.assertEqual(index.index_name, self.index_name)
 
     def test_3_get_task_status(self):
         """
         Test get task status.
         """
 
-        task_status = self.client.index.get_task_status(TestIndex._get_index_id())
+        task_status = self.client.index.get_task_status(self.index_id)
         self.logger.debug(f"Task status: {task_status}")
 
-        self.assertIsNotNone(task_status)
-        self.assertEqual(task_status.index_id, TestIndex._get_index_id())
+        if task_status is not None:
+            self.assertEqual(task_status.index_id, self.index_id)
 
     def test_4_list_indexes(self):
         """
@@ -98,10 +84,10 @@ class TestIndex(unittest.TestCase):
         """
 
         new_index_name = self.index_name + "_updated"
-        is_updated = self.client.index.update(TestIndex._get_index_id(), new_index_name)
+        is_updated = self.client.index.update(self.index_id, new_index_name)
         self.assertTrue(is_updated)
 
-        index = self.client.index.get(TestIndex._get_index_id())
+        index = self.client.index.get(self.index_id)
         self.logger.debug(f"Index: {index}")
         self.assertEqual(index.index_name, new_index_name)
 
@@ -110,10 +96,10 @@ class TestIndex(unittest.TestCase):
         Test delete index.
         """
 
-        is_deleted = self.client.index.delete(TestIndex._get_index_id())
+        is_deleted = self.client.index.delete(self.index_id)
         self.assertTrue(is_deleted)
 
-        index = self.client.index.get(TestIndex._get_index_id())
+        index = self.client.index.get(self.index_id)
         self.assertTrue(index.deleted)
 
 
